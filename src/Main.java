9ce82980b0c2e2ccb69c073140c5f3f0cc46df33
Anastasiya Kobeleva <anastasiya.kobeleva.95@gmail.com>
2022-10-29 14:14:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int prise = 7500;
+        int priseMile = 20;
+        int mile = prise/priseMile;
+        System.out.println("Начислено миль за покупку билета - "+mile);
+    }
+}
